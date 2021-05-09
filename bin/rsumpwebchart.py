547b0de8b74f44c0eff23cae4285b2df99f1400d
bdevy <brian.sagrillo+github@gmail.com>
2021-05-08 21:57:31 -0500

    Fix temperature graphs.

--- bin/rsumpwebchart.py
+++ bin/rsumpwebchart.py
@@ -9,7 +9,12 @@
 
 import time
 from raspisump import webchart
+import configparser
 
+config = configparser.RawConfigParser()
+
+config.read('/home/pi/raspi-sump/raspisump.conf')
+monitor_temp = config.getint('pit', 'monitor_temperature')
 
 def main():
     '''Pass variables to webchart.py'''
@@ -18,8 +23,14 @@ def main():
     today = time.strftime('%Y%m%d')
     homedir = '/home/pi/raspi-sump/'
     webchart.create_folders(year, month, homedir)
-    webchart.create_chart(homedir)
-    webchart.copy_chart(year, month, today, homedir)
+ 
+    webchart.create_chart(homedir, 'waterlevel')
+    webchart.copy_chart(year, month, today, homedir, 'waterlevel')
+
+    if monitor_temp:
+        webchart.create_chart(homedir, 'watertemp')
+        webchart.copy_chart(year, month, today, homedir, 'watertemp')
+
 
 if __name__ == '__main__':
     main()
