bfe7fc411c02b0a64661f49893ddd2702c8a4711
bdevy <brian.sagrillo+github@gmail.com>
2021-05-21 23:49:25 -0500

    consolidated 2 log_reading functions for depth & temp into one function.

--- raspisump/log.py
+++ raspisump/log.py
@@ -10,27 +10,16 @@
 import time
 
 
-def log_reading(water_depth):
+def log_reading(value, csv_name):
     '''Log time and water depth reading.'''
     time_of_reading = time.strftime("%H:%M:%S,")
-    filename = "/home/pi/raspi-sump/csv/waterlevel-{}.csv".format(
+    filename = "/home/pi/raspi-sump/csv/{}-{}.csv".format(
+        csvname,
         time.strftime("%Y%m%d")
     )
     csv_file = open(filename, 'a')
     csv_file.write(time_of_reading),
-    csv_file.write(str(water_depth)),
-    csv_file.write("\n")
-    csv_file.close()
-
-def log_reading_temp(water_temp):
-    '''Log time and water temp reading.'''
-    time_of_reading = time.strftime("%H:%M:%S,")
-    filename = "/home/pi/raspi-sump/csv/watertemp-{}.csv".format(
-        time.strftime("%Y%m%d")
-    )
-    csv_file = open(filename, 'a')
-    csv_file.write(time_of_reading),
-    csv_file.write(str(water_temp)),
+    csv_file.write(str(value)),
     csv_file.write("\n")
     csv_file.close()
 
