a276e412938d6c7dbadd0b6d956e08168a231a73
bdevy <brian.sagrillo+github@gmail.com>
2021-05-21 23:48:33 -0500

    Added store_values() function; additional logic for collecting depth/temp metrics

--- bin/rsump.py
+++ bin/rsump.py
@@ -19,12 +19,44 @@ config = configparser.RawConfigParser()
 
 config.read('/home/pi/raspi-sump/raspisump.conf')
 reading_interval = config.getint('pit', 'reading_interval')
+monitor_temp = config.getint('pit', 'monitor_temperature')
 
 if reading_interval == 0:
-    reading.water_depth()
-    reading.temp_reading()
+    depth = reading.water_depth()
+    values = {
+        "depth": depth
+    }
+    if monitor_temp:
+        values["temp"] = reading.temp_reading()
+
+    store_values(values)
 else:
     while True:
-        reading.water_depth()
-        reading.temp_reading()
+        depth = reading.water_depth()
+        values = {
+            "depth": depth
+        }
+
+        if monitor_temp:
+            values["temp"] = reading.temp_reading()
+
+        store_values(values)
         time.sleep(reading_interval)
+
+
+def store_values(values):
+    ''' Decide which storage mechanism to use. '''
+
+    if use_csv:
+        storage.csv_writer(values[depth], "waterlevel")
+
+        if monitor_temp:
+            storage.csv_writer(values[temp], "watertemp")
+
+    if use_zabbix:
+        #storage.zabbix_writer(values)
+        pass
+
+    if use_influxdb:
+        storage.influxdb_writer(values)
+
